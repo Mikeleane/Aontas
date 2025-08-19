1f98e92e0805c0b69679b25c568b30b4fa9fa4bb
Mikeleane <servilingual@gmail.com>
2025-08-19 21:36:35 +0200

    config

--- apps/worksheets/app/page.tsx
+++ apps/worksheets/app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 
+// prevent static pre-render; always run on request
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   redirect("/embed");
 }
-
